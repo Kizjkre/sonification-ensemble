2fc3083ebe7dc97ba0b5a9b229bcbed0ef124438
Tristan <24882191+Kizjkre@users.noreply.github.com>
2024-07-01 16:55:38 -0700

    Add metadata and create 0.0.1-alpha build

--- src/lib/components/Title.svelte
+++ src/lib/components/Title.svelte
@@ -1,1 +1,5 @@
-<h1 class="flex font-display justify-end pb-4 self-end text-5xl">Sonification Ensemble.</h1>
+<section class="flex flex-col items-end justify-end pb-4">
+  <h1 class="font-display text-5xl">Sonification Ensemble.</h1>
+  <!--suppress JSUnresolvedReference -->
+  <h6 class="text-xs">Version { meta.version }, built 07-01-2024.</h6>
+</section>
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,12 @@
+import { readFileSync } from 'fs';
+// @ts-ignore
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  plugins: [sveltekit()]
+  plugins: [sveltekit()],
+  // REF: https://stackoverflow.com/a/72141502
+  define: {
+    meta: { version: JSON.parse(readFileSync('package.json', 'utf8')).version }
+  }
 });
