5a37949c37c658a1b9203eb21dcabe58e2fbdc04
Tristan <24882191+Kizjkre@users.noreply.github.com>
2024-07-24 18:06:37 -0700

    Add SSL for local development

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,10 @@ import { readFileSync } from 'fs';
 // @ts-ignore
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import viteBasicSslPlugin from '@vitejs/plugin-basic-ssl';
 
 export default defineConfig({
-  plugins: [sveltekit()],
+  plugins: [viteBasicSslPlugin(), sveltekit()],
   // REF: https://stackoverflow.com/a/72141502
   define: {
     meta: { version: JSON.parse(readFileSync('package.json', 'utf8')).version }
