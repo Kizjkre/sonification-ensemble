69ac2f77ac3e5ca6d86acdfa54227680e5c47482
Tristan <24882191+Kizjkre@users.noreply.github.com>
2024-08-18 02:49:38 -0700

    Fix bug where GUI could only start and not stop

--- src/lib/components/Menu.svelte
+++ src/lib/components/Menu.svelte
@@ -3,7 +3,7 @@
   import Upload from '$lib/components/Upload.svelte';
   import selected from '$lib/stores/selected.js';
   import state, { STATE } from '$lib/stores/state.js';
-  import sonify from '$lib/utils/sonify.js';
+  import sonify, { reset } from '$lib/utils/sonify.js';
   import { createEventDispatcher } from 'svelte';
 
   const dispatcher = createEventDispatcher();
@@ -14,6 +14,8 @@
     await (await navigator.storage.getDirectory()).remove({ recursive: true });
     window.location.reload();
   };
+
+  const handleToggle = () => $state === STATE.playing ? reset(false) : sonify(false);
 </script>
 
 <section class="gap-1 grid grid-cols-2">
@@ -26,7 +28,7 @@
     class:hover:text-gray-400={ $selected === -1 }
     class:text-gray-400={ $selected === -1 }
     disabled={ $selected === -1 }
-    on:click={ sonify }
+    on:click={ handleToggle }
   >
     { $state === STATE.playing ? 'Stop' : 'Start' } (<kbd class="bg-gray-100 px-1 py-0.5 rounded text-red-400">shift</kbd>)
   </button>
